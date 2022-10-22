7ac321fd8bffc02023dde79e267975d3cd589f94
NittaBot <116015036+NittaBot@users.noreply.github.com>
2022-10-22 15:47:58 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 /*
 › Create By @RIY
-› Base Ori @DikaArdnt
+› Base Ori @RidhoStore
 › Kalau mau tambahin fitur di index.js
 › Edit text menu di file language › help.js
 */
@@ -22,9 +22,9 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['628818613710'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['628818613710'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '628118613710' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
 global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
